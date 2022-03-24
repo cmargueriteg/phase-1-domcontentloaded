ef66978b67a5b2d72f734b7cf5601f27307900c5
Cindy Germain <cindymarguerite@gmail.com>
2022-03-24 13:21:14 -0600

    commit tree f7836d7ba537dea45e06eca89fc7ff0f93a38358

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+  document.addEventListener("DOMContentLoaded", function(){
+
+  let newPhrase = document.querySelector('p')
+
+newPhrase.textContent = `This is really cool!`
+
+    
+
+ 
+
+  }) 
